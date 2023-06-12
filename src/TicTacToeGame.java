2d099d3cf153b063dbf09767a4cde362767ebd31
Zubin Guan <zubing2@nycstudents.net>
2023-06-12 02:54:57 -0400

    FINAL

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,2 +1,183 @@
-package PACKAGE_NAME;public class TicTacToeGame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeGame extends JFrame implements ActionListener {
+    private JButton[][] buttons;
+    private boolean playerX;
+
+    public TicTacToeGame() {
+        setTitle("Tic Tac Toe");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLayout(new GridLayout(3, 3));
+
+        buttons = new JButton[3][3];
+        playerX = true;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+
+        if (button.getText().isEmpty()) {
+            if (playerX) {
+                button.setText("X");
+            } else {
+                button.setText("O");
+            }
+
+            button.setEnabled(false);
+            playerX = !playerX;
+
+            if (checkForWin()) {
+                highlightWinningLine();
+                int option = JOptionPane.showOptionDialog(this, (playerX ? "O" : "X") + " wins!  What now?", "Game Over",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Restart", "Main Menu"}, "Restart");
+
+                if (option == JOptionPane.YES_OPTION) {
+                    resetGame();
+                } else {
+                    returnToMainMenu();
+                }
+            } else if (isBoardFull()) {
+                int option = JOptionPane.showOptionDialog(this, "It's a draw!  Now what?", "Game Over",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Restart", "Main Menu"}, "Restart");
+
+                if (option == JOptionPane.YES_OPTION) {
+                    resetGame();
+                } else {
+                    returnToMainMenu();
+                }
+            }
+        }
+    }
+
+    private boolean checkForWin() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[i][0].getText().isEmpty() &&
+                    buttons[i][0].getText().equals(buttons[i][1].getText()) &&
+                    buttons[i][1].getText().equals(buttons[i][2].getText())) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[0][i].getText().isEmpty() &&
+                    buttons[0][i].getText().equals(buttons[1][i].getText()) &&
+                    buttons[1][i].getText().equals(buttons[2][i].getText())) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (!buttons[0][0].getText().isEmpty() &&
+                buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][2].getText())) {
+            return true;
+        }
+
+        if (!buttons[0][2].getText().isEmpty() &&
+                buttons[0][2].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][0].getText())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void highlightWinningLine() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[i][0].getText().isEmpty() &&
+                    buttons[i][0].getText().equals(buttons[i][1].getText()) &&
+                    buttons[i][1].getText().equals(buttons[i][2].getText())) {
+                buttons[i][0].setBackground(Color.GREEN);
+                buttons[i][1].setBackground(Color.GREEN);
+                buttons[i][2].setBackground(Color.GREEN);
+                return;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[0][i].getText().isEmpty() &&
+                    buttons[0][i].getText().equals(buttons[1][i].getText()) &&
+                    buttons[1][i].getText().equals(buttons[2][i].getText())) {
+                buttons[0][i].setBackground(Color.GREEN);
+                buttons[1][i].setBackground(Color.GREEN);
+                buttons[2][i].setBackground(Color.GREEN);
+                return;
+            }
+        }
+
+        // Check diagonals
+        if (!buttons[0][0].getText().isEmpty() &&
+                buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][2].getText())) {
+            buttons[0][0].setBackground(Color.GREEN);
+            buttons[1][1].setBackground(Color.GREEN);
+            buttons[2][2].setBackground(Color.GREEN);
+            return;
+        }
+
+        if (!buttons[0][2].getText().isEmpty() &&
+                buttons[0][2].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][0].getText())) {
+            buttons[0][2].setBackground(Color.GREEN);
+            buttons[1][1].setBackground(Color.GREEN);
+            buttons[2][0].setBackground(Color.GREEN);
+        }
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+                buttons[i][j].setBackground(null);
+            }
+        }
+        playerX = true;
+    }
+
+    private void returnToMainMenu() {
+        dispose();
+        JFrame frame = new JFrame("Main Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        MainMenu mainMenu = new MainMenu();
+        frame.add(mainMenu);
+
+        // Center the frame on the screen
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
 }
